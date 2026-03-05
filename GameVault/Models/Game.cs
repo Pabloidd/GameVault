4f5b024aec554bcd2d320e3f69bc6ec655cf5920
Pabloid <pkorotkih715@gmail.com>
2026-03-05 18:02:26 +0300

    исправлена проблема получения даты при запросе игр

--- GameVault/Models/Game.cs
+++ GameVault/Models/Game.cs
@@ -1,10 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace GameVault.Models
 {
     public class Game
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Company { get; set; } = string.Empty;
-        public decimal Weight { get; set; }
-        public DateTime ReleaseDate { get; set; }
-    }
+{
+    public string Title { get; set; } = string.Empty;
+    public string Company { get; set; } = string.Empty;
+    public decimal Weight { get; set; }
+    public DateTime release_date { get; set; }  //  как в БД, временный фикс
+}
 }
